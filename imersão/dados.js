711924e274092305662193d19ce1450e7a0e5f8e
murify <143279428+ciscokrik@users.noreply.github.com>
2024-09-08 00:03:45 -0300

    Add files via upload

---
+++ imersão/dados.js
@@ -0,0 +1,50 @@
+let dados = [
+    {
+        titulo: "Gato de Botas 2",
+        descricao: "é uma animação que leva o charmoso felino de Shrek a uma nova jornada épica. Descobrindo que já gastou oito de suas nove vidas, o Gato embarca em uma missão para encontrar a mítica Estrela dos Desejos e restaurar suas vidas perdidas. Com uma pitada de humor, ação e uma trama envolvente, o filme conquista tanto o público infantil quanto adulto, oferecendo uma aventura emocionante e cheia de referências a contos de fadas clássicos.",
+        link: "https://pt.wikipedia.org/wiki/Puss_in_Boots:_The_Last_Wish",
+        tags: "gato gato de botas gato de botas 2"
+    },
+    {
+        titulo: "Evidências",
+        descricao: "O comediante Fábio Porchat investiga casos bizarros e hilários em um reality show de investigação paranormal. Com humor ácido e uma equipe excêntrica, Porchat busca desvendar mistérios e proporcionar muitas risadas ao público.",
+        link: "https://www.youtube.com/channel/UC-12_t8r80aQz45429R3y5A",
+        tags: "fabio porchat evidências evidencias chitãozinho e chororo"
+    },
+    {
+        titulo: "Encanto",
+        descricao: "Uma animação da Disney que conta a história de uma família colombiana mágica, onde cada membro possui um dom especial, exceto a protagonista Mirabel. Em uma jornada para salvar sua família e sua casa mágica, Mirabel descobre o verdadeiro significado de família e amor.",
+        link: "https://www.disney.com/pt-br/movies/encanto",
+        tags: "disney encanto mirabel casa madrigal"
+    },
+    {
+        titulo: "O Poderoso Chefão",
+        descricao: "Um drama épico que narra a história da família Corleone, uma poderosa máfia italiana nos Estados Unidos. O filme acompanha a ascensão de Michael Corleone ao poder e as consequências de suas escolhas.",
+        link: "https://www.imdb.com/title/tt0068646/",
+        tags: "mafia drama familia crime"
+    },
+    {
+        titulo: "A Lista de Schindler",
+        descricao: "Um drama histórico que retrata a história real de Oskar Schindler, um empresário alemão que salvou mais de mil judeus do Holocausto, empregando-os em sua fábrica.",
+        link: "https://www.imdb.com/title/tt0108052/",
+        tags: "holocausto segunda guerra mundial drama histórico"
+    },
+    {
+        titulo: "O Senhor dos Anéis: A Sociedade do Anel",
+        descricao: "Uma épica jornada de um hobbit chamado Frodo Bolseiro e seus companheiros para destruir o Um Anel, uma poderosa arma capaz de controlar a Terra Média.",
+        link: "https://www.imdb.com/title/tt0120737/",
+        tags: "fantasia aventura épico tolkien"
+    },
+    {
+        titulo: "Parasita",
+        descricao: "Um thriller sul-coreano que explora a desigualdade social através da história de duas famílias com realidades muito diferentes, cujos destinos se entrelaçam de forma trágica.",
+        link: "https://www.imdb.com/title/tt6751668/",
+        tags: "thriller drama sul-coreano desigualdade social"
+    },
+    {
+        titulo: "Matrix",
+        descricao: "Um filme de ficção científica que explora a natureza da realidade e a possibilidade de um mundo simulado. Neo, um hacker, descobre a verdade sobre o mundo em que vive e se torna um líder na luta contra as máquinas.",
+        link: "https://www.imdb.com/title/tt0133093/",
+        tags: "ficção científica ação cyberpunk"
+    }
+];
